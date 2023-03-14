c500a17af5a3698d9cec19dc217bcb8d464d141a
Wilmer Arambula <terabytesoftw@gmail.com>
2023-03-14 18:00:29 -0300

    Add dark mode.

--- src/FlowbiteDarkProdAsset.php
+++ src/FlowbiteDarkProdAsset.php
@@ -4,25 +4,25 @@ declare(strict_types=1);
 
 namespace Yii\Flowbite\Asset;
 
-use Yii\Tailwind\Asset\TailwindDevAsset;
+use Yii\Tailwind\Asset\TailwindDarkProdAsset;
 use Yiisoft\Assets\AssetBundle;
 use Yiisoft\Files\PathMatcher\PathMatcher;
 
-final class FlowbiteDevAsset extends AssetBundle
+final class FlowbiteDarkProdAsset extends AssetBundle
 {
     public string|null $basePath = '@assets';
     public string|null $baseUrl = '@assetsUrl';
     public string|null $sourcePath = '@npm/flowbite';
-    public array $css = ['dist/flowbite.css'];
-    public array $js = ['dist/flowbite.js'];
-    public array $depends = [TailwindDevAsset::class];
+    public array $css = ['dist/flowbite.min.css'];
+    public array $js = ['dist/flowbite.min.js'];
+    public array $depends = [TailwindDarkProdAsset::class];
 
     public function __construct()
     {
         $pathMatcher = new PathMatcher();
 
         $this->publishOptions = [
-            'filter' => $pathMatcher->only('**dist/flowbite.css', '**dist/flowbite.js', '**dist/flowbite.js.map'),
+            'filter' => $pathMatcher->only('**dist/flowbite.min.css', '**dist/flowbite.min.js'),
         ];
     }
 }
--- src/FlowbiteProdAsset.php
+++ src/FlowbiteProdAsset.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace Yii\Flowbite\Asset;
 
+use Yii\Tailwind\Asset\TailwindProdAsset;
 use Yiisoft\Assets\AssetBundle;
 use Yiisoft\Files\PathMatcher\PathMatcher;
 
-final class FlowbiteMinAsset extends AssetBundle
+final class FlowbiteProdAsset extends AssetBundle
 {
     public string|null $basePath = '@assets';
     public string|null $baseUrl = '@assetsUrl';
     public string|null $sourcePath = '@npm/flowbite';
     public array $css = ['dist/flowbite.min.css'];
     public array $js = ['dist/flowbite.min.js'];
+    public array $depends = [TailwindProdAsset::class];
 
     public function __construct()
     {
